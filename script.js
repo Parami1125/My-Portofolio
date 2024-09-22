97a20ba8154213d611b3b6488370dd733c07cdf3
Evan Julian <bailmiroya@gmail.com>
2024-09-22 13:28:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,29 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-// Inisialisasi Owl Carousel untuk slider berita
+// Initialize Owl Carousel for news slider
 $(document).ready(function() {
-  $('.news-carousel').owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: true,
-      dots: false,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: true,
-      responsive: {
-          0: {
-              items: 1
-          },
-          576: {
-              items: 2
-          },
-          768: {
-              items: 3
-          },
-          992: {
-              items: 4
-          }
+  $('.owl-carousel').owlCarousel({
+    items: 1, // Adjust number of items based on your needs
+    loop: true,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
       }
+    }
   });
 });
